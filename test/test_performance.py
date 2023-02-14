0c80cb1f1a923ab1e1e6c084dbcec5ad5f5697a2
Toby Devlin <toby@tobydevlin.com>
2023-02-14 22:37:58 +1100

    missed fun call :facepalm: + rejoin test jobs

--- test/test_performance.py
+++ test/test_performance.py
@@ -3,10 +3,10 @@ from data_processing_benchmarks.polars import do_polars_test
 
 
 def test_pandas():
-    res = do_pandas_test
+    res = do_pandas_test()
     assert res is not None
 
 
 def test_polars():
-    res = do_polars_test
+    res = do_polars_test()
     assert res is not None
