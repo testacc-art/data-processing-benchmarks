bd06ec95178351cb8b656a70339b2a8e94690c3f
Toby Devlin <toby@tobydevlin.com>
2023-02-14 22:27:26 +1100

    re add bench for pandas

--- test/test_performance.py
+++ test/test_performance.py
@@ -1,10 +1,10 @@
 from data_processing_benchmarks.pandas import do_pandas_test
 from data_processing_benchmarks.polars import do_polars_test
 
-# def test_pandas(benchmark):
-#     res = benchmark(do_pandas_test)
-#     assert res is not None
-#
+
+def test_pandas(benchmark):
+    res = benchmark(do_pandas_test)
+    assert res is not None
 
 
 def test_polars():
